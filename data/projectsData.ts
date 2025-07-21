e1cdea4da4f9e4ecbfc45b027cdf81c4a48dc7fb
cq <wb-zcq621102@alibaba-inc.com>
2025-07-21 20:33:48 +0800

    fix

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -13,12 +13,10 @@ const projectsData: Project[] = [
     href: 'https://jerryqun.github.io/saas-temple/#/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    title: 'React在线编辑器',
+    description: `基于react17、antd4、monaco-editor实现的在线编辑器`,
     imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: 'https://jerryqun.github.io/playground-react/#/~demos/docs-playground',
   },
 ]
 
